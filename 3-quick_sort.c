532634d20956b02ce7613a63b4e0aed92c0af9a4
VCruz910 <vscruz910@gmail.com>
2022-08-23 23:00:09 +0000

    Created 3-quick_sort.c function with it's time complexity

---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+/**
+ * SWITCHEROO - Swaps two integers in the array.
+ * @IS1: Integer Swap 1.
+ * @IS2: Integer Swap 2.
+ **/
+void SWITCHEROO(int *IS1, int *IS2)
+{
+	int TMP;
+	TMP = *IS1;
+	*IS1 = *IS2;
+	*IS2 = TMP;
+}
+
+/**
+ * QPartition - Gives a PIV Index for quicksorting.
+ * @array: Array to find PIV Index.
+ * @IDXLeft: Left Element Index.
+ * @IDXRight: Right Element Index.
+ * @size: Array size.
+ * Return: PIV Element Index (I + 1).
+ **/
+int QPartitions(int *array, int IDXLeft, int IDXRight, size_t size)
+{
+	int IDXAbove, IDXBelow;
+	int *Pivot = array + IDXRight;
+
+	for (IDXAbove = IDXBelow = IDXLeft; IDXBelow < IDXRight; IDXBelow++)
+	{
+		if (array[IDXBelow] < *Pivot)
+		{
+			if (IDXAbove < IDXBelow)
+			{
+				SWITCHEROO(array + IDXBelow, array + IDXAbove);
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[IDXAbove] > *Pivot)
+	{
+		SWITCHEROO(array + IDXAbove, Pivot);
+		print_array(array, size);
+	}
+	return (IDXAbove);
+}
+
+/**
+ * QRecursion - Helper function.
+ * @array: Array for sorting.
+ * @IDXLeft: Left Element Index.
+ * @IDXRight: Right Element Index.
+ * @size: Array size.
+ **/
+void QRecursion(int *array, int IDXLeft, int IDXRight, size_t size)
+{
+	int PIV;
+
+	if (IDXRight - IDXLeft > 0)
+	{
+		PIV = QPartitions(array, IDXLeft, IDXRight, size);
+		QRecursion(array, IDXLeft, PIV - 1, size);
+		QRecursion(array, PIV + 1, IDXRight, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers
+ * in ascending order using the Quick sort algorithm.
+ * @array: Variable for array of integers.
+ * @size: Array size.
+ **/
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2 || array == NULL)
+	{
+		return;
+	}
+	QRecursion(array, 0, size - 1, size);
+}
